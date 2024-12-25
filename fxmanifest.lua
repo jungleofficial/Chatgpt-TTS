04f25c6e6d61048ceb6f8f95db0e106e47f35594
Jungle_NPC <fhcjcjckfjjfjc@gmail.com>
2024-12-25 11:43:38 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Jungle_NPC'
+description 'Mission IA avec PNJ interactifs utilisant ChatGPT et TTS'
+version '1.0.0'
+
+shared_script '@es_extended/imports.lua'
+
+server_script 'server.lua'
+client_script 'client.lua'
